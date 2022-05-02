e9d4e0bae24968cf718bc6800da5409f8d2fdf3c
Dobos Levente <h044112@stud.u-szeged.hu>
2022-05-02 19:03:29 +0200

    Add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
-  // TODO: Paste firebase object here
   firebase: {
-    projectId: '',
-    appId: '',
-    storageBucket: '',
-    apiKey: '',
-    authDomain: '',
-    messagingSenderId: '',
-    measurementId: '',
+    projectId: 'webfejl-kotprog-2022',
+    appId: '1:678751183172:web:b096febf9e6b69f0cd3711',
+    storageBucket: 'webfejl-kotprog-2022.appspot.com',
+    apiKey: 'AIzaSyAg1LdHddOCGO5ab-lMO6nOx0-0n0pAmco',
+    authDomain: 'webfejl-kotprog-2022.firebaseapp.com',
+    messagingSenderId: '678751183172',
+    measurementId: 'G-HXX2VSM18L',
   },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  // TODO: Paste firebase object here
   firebase: {
-    projectId: '',
-    appId: '',
-    storageBucket: '',
-    apiKey: '',
-    authDomain: '',
-    messagingSenderId: '',
-    measurementId: '',
+    projectId: 'webfejl-kotprog-2022',
+    appId: '1:678751183172:web:b096febf9e6b69f0cd3711',
+    storageBucket: 'webfejl-kotprog-2022.appspot.com',
+    apiKey: 'AIzaSyAg1LdHddOCGO5ab-lMO6nOx0-0n0pAmco',
+    authDomain: 'webfejl-kotprog-2022.firebaseapp.com',
+    messagingSenderId: '678751183172',
+    measurementId: 'G-HXX2VSM18L',
   },
   production: false,
   hostURL: 'http://localhost:4200'
